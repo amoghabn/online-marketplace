0a3b92c9be1794494ed84c95d85b5f01877847a0
Amogha Bheemanakone Narappa <amoghabn@Amoghas-MacBook-Air.local>
2023-06-13 23:43:23 -0700

    setting up product service

--- product-service/src/main/java/com/bn/productservice/service/ProductService.java
+++ product-service/src/main/java/com/bn/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.bn.productservice.service;
 
 import com.bn.productservice.dto.ProductRequest;
+import com.bn.productservice.dto.ProductResponse;
 import com.bn.productservice.model.Product;
 import com.bn.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,6 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
-
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.getName())
@@ -23,5 +25,18 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
