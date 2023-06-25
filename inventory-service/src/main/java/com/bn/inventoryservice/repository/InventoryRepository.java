1ac15be0e3abfaed08db14becf163af4d5f1f38e
amoghabn <amoghabn1996@gmail.com>
2023-06-25 13:03:22 -0700

    implemented inventory service

---
+++ inventory-service/src/main/java/com/bn/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.bn.inventoryservice.repository;
+
+import com.bn.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
---
+++ inventory-service/src/main/java/com/bn/inventoryservice/service/InventoryService.java
@@ -0,0 +1,22 @@
+package com.bn.inventoryservice.service;
+
+
+import com.bn.inventoryservice.repository.InventoryRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+
+    }
+}
