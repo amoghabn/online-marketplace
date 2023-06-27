b3a0dd871e13864ec967dfef04ccb77a7814bb08
amoghabn <amoghabn1996@gmail.com>
2023-06-26 23:22:02 -0700

    moving back all services

---
+++ product-service/src/main/java/com/bn/productservice/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.bn.productservice.controller;
+
+
+import com.bn.productservice.dto.ProductRequest;
+import com.bn.productservice.dto.ProductResponse;
+import com.bn.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
+}
